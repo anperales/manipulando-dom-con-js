1f7b73ee53da9ac5cbcfcf1552d6a9c319053991
Anabell <anperales@gmail.com>
2023-03-01 18:20:35 -0600

    Proyecto terminado de curso manipulando dom con java script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-( () =>{
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
     const btn = document.querySelector("[data-form-btn]");
     const createTask = (evento) =>{
     evento.preventDefault();
@@ -14,28 +15,8 @@
     titleTask.innerText = value;
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    //backticks
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    //task.innerHTML = content;
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 }
 btn.addEventListener("click", createTask);
-
-//Arrow functions o funciones anónimas
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener("click", completeTask);
-    return i;
-}
-
-// Immediatly invoked function expression IIFE
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-}
-})();
